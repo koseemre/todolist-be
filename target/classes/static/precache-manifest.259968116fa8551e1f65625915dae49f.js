939da4494e4e643e8552b5d7a94f63cd2a5d5bfb
emre <kose.emre773@gmail.com>
2019-07-14 20:12:35 +0300

    fix

---
+++ target/classes/static/precache-manifest.259968116fa8551e1f65625915dae49f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8b9ef76ef18b7e682d2f",
+    "url": "/static/js/main.8b9ef76e.chunk.js"
+  },
+  {
+    "revision": "61d9a32eeb4982eb4a46",
+    "url": "/static/js/1.61d9a32e.chunk.js"
+  },
+  {
+    "revision": "8b9ef76ef18b7e682d2f",
+    "url": "/static/css/main.a00fb637.chunk.css"
+  },
+  {
+    "revision": "61d9a32eeb4982eb4a46",
+    "url": "/static/css/1.7c45ed78.chunk.css"
+  },
+  {
+    "revision": "5b83d17ee67249d3fbb08e6d3925b2ec",
+    "url": "/index.html"
+  }
+];
